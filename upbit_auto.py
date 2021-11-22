0a9293b9d40d9b2e254d5618f4ef8223a9996fb4
uminho1 <94767893+uminho1@users.noreply.github.com>
2021-11-22 19:46:57 +0900

    Update upbit_auto.py

--- upbit_auto.py
+++ upbit_auto.py
@@ -21,78 +21,100 @@ def price_ma():
     ma20_ = round(ma20.iloc[-1], 2)    
     return ma20_
 
-
-# 객체 생성
+# -----------------------------------------------------------------
 access = "nehpcdrsANEdzmeHeWY5MVEElxY4Exl4Y5HymcsH"
 secret = "pH5hvBYyC2wvchkGTyz8gYYGsBvSgv0ZGFMPh66Z"
 upbit =  pyupbit.Upbit(access, secret)
-
-#변수 생성
-target_20 = price_ma()                                                  # 목표가
-op_mode = True                                                          # 거래 가능 상태  
-hold = False                                                            # 코인 보유 상태
+# -----------------------------------------------------------------
+op_mode = True
+hold = False
 count = 1
-sell_price1 = pyupbit.get_current_price("KRW-ETH") * 0.0015              # 2차 매수 목표가격 갱신 (매도가격 * 0.05) + 매도가격   / 1차는 현재가 매수 조건입니다.
+sell_price1 = pyupbit.get_current_price("KRW-ETH") * 0.0015
 sell_price2 = pyupbit.get_current_price("KRW-ETH") + sell_price1
+# -----------------------------------------------------------------
 
 while True:
     now = datetime.datetime.now()
-    krw_balance = round(upbit.get_balance("KRW"), 0)                                  # 잔고확인    
-    krw_call_price = round(upbit.get_avg_buy_price("KRW-ETH"), 0)                     # 매수금액    
-    price = round(pyupbit.get_current_price("KRW-ETH"), 0)                            # 현재가격    
-    price_gap = price - target_20                                                     # 현재가격과 목표가의 차이
+    target_10 = price_ma() 
+    #target_10 = round(pyupbit.get_ohlcv("KRW-ETH", "minute10"), 0)
+    krw_balance = round(upbit.get_balance("KRW"), 0)
+    krw_call_price = round(upbit.get_avg_buy_price("KRW-ETH"), 0)
+    price = round(pyupbit.get_current_price("KRW-ETH"), 0)
+    price_gap = price - target_10
     
-    #====================================================================================================
-    target_20 = price_ma()                                                            # MA20 
-    target_20_call = target_20 - (target_20 * 0.008)                                  # 매수 목표가 (MA20보다 0.008 하락시 매수)
-    target_20_call_gap = target_20 * 0.008                                            # 매수 목표가 Gap
-    #====================================================================================================
-    #====================================================================================================
-    target_20 = price_ma()                                                            # MA20 
-    target_20_down = target_20 - (target_20 * 0.007)                                  # 손절 MA20 목표가
-    target_20_down_gap = target_20 * 0.007                                            # 손절 MA20 목표가 Gap    
+    # 1st_price_value
+    target_10 = target_10
+    target_10_call = target_10 - (target_10 * 0.008)
+    target_10_call_gap = target_10 * 0.004
     #====================================================================================================    
+    # - sell value
+    target_10 = target_10
+    target_10_down = target_10 - (target_10 * 0.007)
+    target_10_down_gap = target_10 * 0.007
     #====================================================================================================    
-    target_20_up = krw_call_price + (krw_call_price * 0.015)                          # 익절 목표가
-    target_20_up_gap = krw_call_price * 0.05                                          # 익절 목표가 Gap     
+    # + 1.5% sell value
+    target_10_up = krw_call_price + (krw_call_price * 0.015)
+    target_10_up_gap = krw_call_price * 0.05
     #====================================================================================================
         
-    # [매수] 조건 시도 (현재가격이 10분봉 MA20보다 낮고, 매수목표가보다 낮으면 매수)
-    if price is not None and price < target_20 and price < target_20_call:
-        krw_balance = upbit.get_balance("KRW")                                        # 잔고확인 
-        upbit.buy_market_order("KRW-ETH", krw_balance)                                # 원화잔고 전체로 시장가로 매수 krw_balance * 0.5 <-- 50%만 매수
-        call_now = datetime.datetime.now()                                            # 매수시간  
-    
-    # [손절] 현재가격이 10분봉 MA20선 아래에 있고, 그리고 MA20선보다 0.005% 하락한 가격이면 손절
-    if price < target_20 and price <= target_20_down:   
-        btc_balance = upbit.get_balance("KRW-ETH")                                        # 잔고확인
-        upbit.sell_market_order("KRW-ETH", btc_balance)                                   # 시장가 매도
-        krw_balance = upbit.get_balance("KRW")                                            # 매도후 잔고확인
-        sell_price1 = pyupbit.get_current_price("KRW-ETH")                                # 매도한 가격
-        sell_now = datetime.datetime.now()                                                # 매도한 시간        
+    # 1st_price_value 
+    if price is not None and count == 1 and price < target_10 and price < target_10_call:
+        krw_balance = upbit.get_balance("KRW")
+        upbit.buy_market_order("KRW-ETH", krw_balance * 0.3)
+        call_now = datetime.datetime.now()
+        price_1st = price
+        count = 2
+        # 2nd_price_value    
+        target_10_call_2nd = price_1st - (price_1st * 0.003)
+        target_10_call_2nd__gap = price_1st * 0.00    
 
-    # [익절] 현재가격이 10분봉 MA20선 위에 있고, 매수가 보다 수익 1.5%이면 익절
-    if price > target_20 and price >= target_20_up:   
-        btc_balance = upbit.get_balance("KRW-ETH")                                        # 잔고확인
-        upbit.sell_market_order("KRW-ETH", btc_balance)                                   # 시장가 매도
-        krw_balance = upbit.get_balance("KRW")                                            # 매도후 잔고확인
-        sell_price1 = pyupbit.get_current_price("KRW-ETH")                                # 매도한 가격
-        sell_now = datetime.datetime.now()                                                # 매도한 시간            
+    # 2nd_price_value 
+    if price is not None and count == 2 and price < target_10 and price < target_10_call_2nd:
+        krw_balance = upbit.get_balance("KRW")
+        upbit.buy_market_order("KRW-ETH", krw_balance * 0.3)
+        call_now = datetime.datetime.now()
+        price_2nd = price
+        count = 3
+        # 3rd_price_value    
+        target_10_call_3rd = price_2nd - (price_2nd * 0.003)
+        target_10_call_3rd__gap = price_1st * 0.00
+
+    # 3rd_price_value 
+    if price is not None and count == 3 and price < target_10 and price < target_10_call_3rd:
+        krw_balance = upbit.get_balance("KRW")
+        upbit.buy_market_order("KRW-ETH", krw_balance)
+        call_now = datetime.datetime.now()
+        price_2nd = price        
+    
+    # - sell value
+    if price < target_10 and price <= target_10_down:   
+        btc_balance = upbit.get_balance("KRW-ETH")
+        upbit.sell_market_order("KRW-ETH", btc_balance)
+        krw_balance = upbit.get_balance("KRW")
+        sell_price1 = pyupbit.get_current_price("KRW-ETH")
+        sell_now = datetime.datetime.now()
+
+    # + 1.5% sell value
+    if price > target_10 and price >= target_10_up:   
+        btc_balance = upbit.get_balance("KRW-ETH")
+        upbit.sell_market_order("KRW-ETH", btc_balance)
+        krw_balance = upbit.get_balance("KRW")
+        sell_price1 = pyupbit.get_current_price("KRW-ETH")
+        sell_now = datetime.datetime.now()
 
     print(f"---------------------------------------------------------")
-    print(now.strftime('▶ 시간: %y/%m/%d %H:%M:%S'))
-    print("▶ MA20: {0:,.0f}".format(target_20))
-    print("▶ price (현재가): {0:,.0f}".format(price))
-    print("▶ price MA Gap (MA차이): {0:,.0f}".format(price_gap))
-    print("▶ - sell(손절가격): {0:,.0f}".format(target_20_down))
-    print("▶ - sell Gap (손절가격Gap): {0:,.0f}".format(target_20_down_gap))
-    print("▶ + sell (익절가격1.5%): {0:,.0f}".format(target_20_up))
-    print("▶ sell Gap (익절가격Gap): {0:,.0f}".format(target_20_up_gap))
-    print("▶ call Target (매수목표가): {0:,.0f}".format(target_20_call))
-    print("▶ call Gap (매수목표가Gap): {0:,.0f}".format(target_20_call_gap))
-    print("▶ call Price (매수한금액): {0:,.0f}".format(krw_call_price))
-    print("▶ Upbit KRW (잔고현황): {0:,.0f}".format(krw_balance))
-    print(f"---------------------------------------------------------")
-    print(f"---------------------------------------------------------")
+    print(now.strftime('▶ Time: %y/%m/%d %H:%M:%S'))
+    print("▶ MA20: {0:,.0f}".format(target_10))
+    print("▶ price : {0:,.0f}".format(price))
+    print("▶ price MA Gap : {0:,.0f}".format(price_gap))
+    print("▶ - sell : {0:,.0f}".format(target_10_down))
+    print("▶ - sell : {0:,.0f}".format(target_10_down_gap))
+    print("▶ + sell : {0:,.0f}".format(target_10_up))
+    print("▶ sell Gap : {0:,.0f}".format(target_10_up_gap))
+    print("▶ call Target : {0:,.0f}".format(target_10_call))
+    print("▶ call Gap : {0:,.0f}".format(target_10_call_gap))    
+    print("▶ call Price : {0:,.0f}".format(krw_call_price))
+    print("▶ Upbit KRW : {0:,.0f}".format(krw_balance))
+    print(f"---------------------------------------------------------")    
     
-    time.sleep(30)
+    time.sleep(30)  
